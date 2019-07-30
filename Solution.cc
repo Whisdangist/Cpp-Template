ee913ed0c9e64c430467d6c5eb9bd6aed4778044
Whisdangist <whisdangist@qq.com>
2019-07-30 21:25:34 +0800

    first commit

---
+++ Solution.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+	cout << "Hello World!" << endl;
+}
+
+int main() {
+	#ifdef OFFLINE
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+	#endif
+
+	solve();
+	
+	#ifdef OFFLINE
+	system("pause");
+	#endif
+	return 0;
+}
