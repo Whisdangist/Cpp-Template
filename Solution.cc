f83dc3ea41322038ca5b1abef486d82825b820e9
Whisdangist <whisdangist@qq.com>
2020-03-13 11:37:04 +0800

    linux first commit

---
+++ Solution.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+	cout << "hello world!" << endl;
+}
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+	solve();
+}
